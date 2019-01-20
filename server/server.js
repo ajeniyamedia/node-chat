12ff07c2910de0b084e6fac0a91a6c87e73e7c04
Oladimeji Ajeniya <oladimejiajeniya@gmail.com>
2019-01-20 21:07:49 +0100

    Emit newMessage on welcome to all

--- server/server.js
+++ server/server.js
@@ -14,6 +14,20 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    // socket.emit from Admin text Welcome to the chat app 
+    socket.emit('newMessage', {
+        from: 'Amin',
+        text: 'Welcome to the chat',
+        createdAt: new Date().getTime()
+    });
+
+    // socket.broadcast.emit from Admin New joined
+    socket.broadcast.emit('newMessage', {
+        from: 'Amin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+
 
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
@@ -22,6 +36,12 @@ io.on('connection', (socket) => {
             text: message.text,
             createdAt: new Date().getTime()
         });
+
+        // socket.broadcast.emit('newMessage',{
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
 
     socket.on('disconnect', () => {
